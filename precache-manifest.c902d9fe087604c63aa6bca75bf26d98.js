e9211f3a4dc48b95004422f1f0ba609030415bff
Harshil Patel <harshil4076@gmail.com>
2020-05-29 11:52:42 -0400

    Updates

---
+++ precache-manifest.c902d9fe087604c63aa6bca75bf26d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de0bdbef40028b92cd60787464b8155",
+    "url": "/color_project/index.html"
+  },
+  {
+    "revision": "8f54754c5a8e4054de7a",
+    "url": "/color_project/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "1a89e9af219ad25a2cfc",
+    "url": "/color_project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8f54754c5a8e4054de7a",
+    "url": "/color_project/static/js/2.fac3faf1.chunk.js"
+  },
+  {
+    "revision": "1a89e9af219ad25a2cfc",
+    "url": "/color_project/static/js/main.8f0d9804.chunk.js"
+  },
+  {
+    "revision": "b06cc4d483500bef33ae",
+    "url": "/color_project/static/js/runtime-main.e2da7191.js"
+  }
+]);
